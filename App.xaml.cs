7504f49ea4a21b7d1e27f10d3466c11ff5fdc774
vmadmin <vmadmin@WIV11-VMWP>
2024-11-08 10:11:19 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WanjaHaldemann_Projektauftrag_PROG1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
